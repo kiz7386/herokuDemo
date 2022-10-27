4ec0c7d054f36f32b957d456be7dace43dd1275f
Kiz Tw <kiztw@TW-Kiz-MBP.local>
2022-10-27 12:10:57 +0800

    Update put url

--- src/main/java/com/example/herokudemo/constant/Constants.java
+++ src/main/java/com/example/herokudemo/constant/Constants.java
@@ -3,5 +3,5 @@ package com.example.herokudemo.constant;
 public class Constants {
 
     public static final String TELEGRAM_URL ="https://api.telegram.org/";
-    public static final String PTT_URL ="https://ptt.cc";
+    public static final String PTT_URL ="http://ptt.cc";
 }
