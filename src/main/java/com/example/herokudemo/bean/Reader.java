eeffd20dd1fdac8a3a4a8f0749c5521eb03eba3b
Kiz Tw <kiztw@TW-Kiz-MBP.local>
2022-10-31 11:03:32 +0800

    Hidd init method

--- src/main/java/com/example/herokudemo/bean/Reader.java
+++ src/main/java/com/example/herokudemo/bean/Reader.java
@@ -24,48 +24,48 @@ public class Reader {
     private Map<String, List<Cookie>> cookieStore; // 保存 Cookie
     private CookieJar cookieJar;
 
-    @PostConstruct
-    public void init() throws Exception{
-        Response response =null;
-        try{
-
-
-        /* 初始化 */
-        cookieStore = new HashMap<>();
-        cookieJar = new CookieJar() {
-            /* 保存每次伺服器端回傳的 Cookie */
-            @Override
-            public void saveFromResponse(HttpUrl httpUrl, List<Cookie> list) {
-                List<Cookie> cookies = cookieStore.getOrDefault(
-                        httpUrl.host(),
-                        new ArrayList<>()
-                );
-                cookies.addAll(list);
-                cookieStore.put(httpUrl.host(), cookies);
-            }
-
-            /* 每次發送帶上儲存的 Cookie */
-            @Override
-            public List<Cookie> loadForRequest(HttpUrl httpUrl) {
-                return cookieStore.getOrDefault(
-                        httpUrl.host(),
-                        new ArrayList<>()
-                );
-            }
-        };
-        okHttpClient = okHttpClient.newBuilder().cookieJar(cookieJar).build();
-
-        /* 獲得網站的初始 Cookie */
-        Request request = new Request.Builder().get().url(Config.PTT_URL).build();
-        response = okHttpClient.newCall(request).execute();
-        } catch (Exception e){
-            e.printStackTrace();
-        } finally {
-            if(response != null){
-                response.close();
-            }
-        }
-    }
+//    @PostConstruct
+//    public void init() throws Exception{
+//        Response response =null;
+//        try{
+//
+//
+//        /* 初始化 */
+//        cookieStore = new HashMap<>();
+//        cookieJar = new CookieJar() {
+//            /* 保存每次伺服器端回傳的 Cookie */
+//            @Override
+//            public void saveFromResponse(HttpUrl httpUrl, List<Cookie> list) {
+//                List<Cookie> cookies = cookieStore.getOrDefault(
+//                        httpUrl.host(),
+//                        new ArrayList<>()
+//                );
+//                cookies.addAll(list);
+//                cookieStore.put(httpUrl.host(), cookies);
+//            }
+//
+//            /* 每次發送帶上儲存的 Cookie */
+//            @Override
+//            public List<Cookie> loadForRequest(HttpUrl httpUrl) {
+//                return cookieStore.getOrDefault(
+//                        httpUrl.host(),
+//                        new ArrayList<>()
+//                );
+//            }
+//        };
+//        okHttpClient = okHttpClient.newBuilder().cookieJar(cookieJar).build();
+//
+//        /* 獲得網站的初始 Cookie */
+//        Request request = new Request.Builder().get().url(Config.PTT_URL).build();
+//        response = okHttpClient.newCall(request).execute();
+//        } catch (Exception e){
+//            e.printStackTrace();
+//        } finally {
+//            if(response != null){
+//                response.close();
+//            }
+//        }
+//    }
 
     public List<Article> getList(String boardName) throws IOException, ParseException, ParseException {
         Response response = null;
